ec04ffce6bc07ffdc72d75e32b43e4c99b1e5116
OmGuruGaneshaKOnduri <OmGuruGaneshaKonduri@my.unt.edu>
2024-10-18 21:48:36 -0500

    updated the server with get warrantys api endpoint

---
+++ server/controllers/warrantyControllers.js
@@ -0,0 +1,65 @@
+const addWarranty = async (req, res) => {
+    const { userId } = req.params;
+  
+    try {
+      if (!userId) {
+        return res.status(200).json({ missingId: "User ID is required" });
+      }
+  
+      const user = await User.findById(userId);
+  
+      if (!user) {
+        return res.status(200).json({ userNotFound: "User not found" });
+      }
+  
+      const { purchaseDate, warrantyPeriod, purchaseAddress } = req.body;
+  
+      if (!purchaseDate || !warrantyPeriod || !purchaseAddress) {
+        return res
+          .status(200)
+          .json({
+            missingFields:
+              "Purchase date, warranty period, and purchase address are required",
+          });
+      }
+  
+      user.warranty.push({
+        purchaseDate,
+        warrantyPeriod,
+        purchaseAddress,
+      });
+  
+      await user.save();
+  
+      return res
+        .status(200)
+        .json({ warrantyAdded: "Warranty added successfully" });
+    } catch (error) {
+      console.error(error);
+      return res
+        .status(500)
+        .json({ error: "An error occurred while adding the warranty" });
+    }
+  };
+
+  async function getWarranty(req, res) {
+    const { userId } = req.params;
+    try {
+      if (!userId) {
+        return res.status(200).json({ missingId: "User ID is required" });
+      }
+  
+      const user = await User.findById(userId);
+  
+      if (!user) {
+        return res.status(200).json({ userNotFound: "User not found" });
+      }
+  
+      return res.status(200).json({ warranty: user.warranty });
+    } catch (error) {
+      console.error(error);
+      return res
+        .status(500)
+        .json({ error: "An error occurred while getting the warranty" });
+    }
+  }
