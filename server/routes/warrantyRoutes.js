1a3c75a4b6a6596006679b50d040f0a650fa2a81
Surya Tejas Reddy Nimma <107686299+surya663@users.noreply.github.com>
2024-11-22 21:30:21 -0600

    update the warranty routes in the server

--- server/routes/warrantyRoutes.js
+++ server/routes/warrantyRoutes.js
@@ -1,11 +1,13 @@
 const express = require("express");
-const { addWarranty, getWarranty } = require("../Controllers/WarrantyController");
+const { addWarranty, getWarranty, getWarrantyById } = require("../Controllers/WarrantyController");
 const router = express.Router();
 
 
 router.post("/claim-warranty/:userId", addWarranty);
 
 router.get("/warranty/:userId", getWarranty);
+
+router.get("/product/:productId", getWarrantyById);
 
  
 module.exports = router;
