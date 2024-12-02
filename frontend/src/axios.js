f2d061a685524e0f888dfabf0a82ff118c77c6d1
KalisettiRamyaSudha <kramyasudha2024@gmail.com>
2024-12-02 23:52:39 +0530

    Update axios.js

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -1,10 +1,12 @@
 import axios from "axios";
 
+const publicIP = '<public-ip>' // elastic IP of development server
 
-const BASE_URL = "http://localhost:4003/api" 
+const BASE_URL = `http://${publicIP}:4003/api`
+
 
 const axiosInstance=axios.create({
     baseURL:BASE_URL,
 });
 
-export default axiosInstance;   
+export default axiosInstance; 
