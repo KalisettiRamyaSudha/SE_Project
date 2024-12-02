26f8a5fc6afce38becbc3b66088e0ae326168fb6
KalisettiRamyaSudha <kramyasudha2024@gmail.com>
2024-12-02 12:18:13 -0600

    updated code repo

--- backend/Routes/ProductRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const { ProductController } = require("../Controllers/ProductController");
-const router = express.Router();
-
-
-router.get("/product/:productId", ProductController);
-
-module.exports = router
